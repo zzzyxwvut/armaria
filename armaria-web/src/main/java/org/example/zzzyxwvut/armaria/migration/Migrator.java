2db7f725cfb6c09bf0cffb2ba2b2c4c9df8d61fd
Aliaksei Budavei <oytis@yandex.ru>
2017-12-21 22:58:36 +0300

    Create a Migrator class
    This Migrator class should derive from the HttpServlet class and
    employ the Flyway tool to migrate data to a database.

---
+++ armaria-web/src/main/java/org/example/zzzyxwvut/armaria/migration/Migrator.java
@@ -0,0 +1,60 @@
+package org.example.zzzyxwvut.armaria.migration;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.flywaydb.core.Flyway;
+
+public class Migrator extends HttpServlet
+{
+	private static final long serialVersionUID	= 2777568529926894959L;
+	private final Logger logger	= LogManager.getLogger();
+
+	public Migrator() { }
+
+	/*
+	 * From https://flywaydb.org/documentation/migrations
+	 *
+	 * The file name consists of the following parts:
+	 *
+	 * Prefix: V for versioned migrations,
+	 *	U for undo migrations,
+	 *	R for repeatable migrations
+	 *
+	 * Version: Underscores (automatically replaced by dots at runtime)
+	 *	separate as many parts as you like (not for repeatable migrations)
+	 *
+	 * Separator: __ (two underscores)
+	 *
+	 * Description: Underscores (automatically replaced by spaces at runtime)
+	 *	separate the words
+	 */
+	@Override
+	public void init() throws ServletException
+	{
+		try {
+			Context context	= new InitialContext();
+			DataSource s	= (DataSource)
+					context.lookup("java:/comp/env/jdbc/poachers");
+			Flyway flyway	= new Flyway();
+			flyway.setDataSource(s);
+			flyway.baseline();
+			int score	= flyway.migrate();
+
+			if (score > 0) {
+				logger.debug(new StringBuilder(24)
+					.append("Applied ").append(score)
+					.append(" migrations.").toString());
+			} else {
+				logger.debug("Database is up to date.");
+			}
+		} catch (Exception e) {
+			logger.error(Migrator.class.getCanonicalName(), e);
+		}
+	}
+}
