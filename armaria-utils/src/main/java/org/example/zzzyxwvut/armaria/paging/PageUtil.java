ee25bb090e6225de6accb9dcf1737c608a8c8124
Aliaksei Budavei <oytis@yandex.ru>
2018-01-10 19:36:58 +0300

    Write missing Javadoc comments

--- armaria-utils/src/main/java/org/example/zzzyxwvut/armaria/paging/PageUtil.java
+++ armaria-utils/src/main/java/org/example/zzzyxwvut/armaria/paging/PageUtil.java
@@ -1,5 +1,8 @@
 package org.example.zzzyxwvut.armaria.paging;
 
+/**
+ * This class manages common page composition tasks.
+ */
 public class PageUtil
 {
 	private static int begin	= -1;
@@ -11,6 +14,16 @@ public class PageUtil
 		throw new AssertionError();
 	}
 
+	/**
+	 * Calculates the sub-range of values.
+	 *
+	 * @param firstPos	the minimal value of a range
+	 * @param lastPos	the maximal value of a range
+	 * @param currentPos	the current value within range
+	 * @param itemsPerView	the number of values to make a slice
+	 * @throws	IllegalArgumentException if any argument is not
+	 *			a natural number or is misplaced
+	 */
 	public static void makeSlice(final int firstPos, final int lastPos,
 				final int currentPos, final int itemsPerView)
 	{
@@ -33,6 +46,17 @@ public class PageUtil
 		}
 	}
 
+	/**
+	 * Gets the minimal value of a sub-range.
+	 *
+	 * @return	the minimal value of a sub-range
+	 */
 	public static int getBegin()	{ return PageUtil.begin; }
+
+	/**
+	 * Gets the maximal value of a sub-range.
+	 *
+	 * @return	the maximal value of a sub-range
+	 */
 	public static int getEnd()	{ return PageUtil.end; }
 }
