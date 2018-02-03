983f5f9f7dd3a8c7bf126c8166586cca00698059
Aliaksei Budavei <oytis@yandex.ru>
2018-02-03 23:26:13 +0300

    Rid the IndexController class of the exception handling

--- armaria-web/src/main/java/org/example/zzzyxwvut/armaria/controllers/IndexController.java
+++ armaria-web/src/main/java/org/example/zzzyxwvut/armaria/controllers/IndexController.java
@@ -1,24 +1,13 @@
 package org.example.zzzyxwvut.armaria.controllers;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(produces = "text/html; charset=UTF-8")
 public class IndexController
 {
-	private final Logger logger	= LogManager.getLogger();
-
-	@ExceptionHandler(Exception.class)
-	private String handle(Exception e)
-	{
-		logger.error(e.getMessage(), e);
-		return "index";
-	}
-
-	@RequestMapping(value = {"/", "/index"}, produces = "text/html; charset=UTF-8")
+	@RequestMapping(value = {"/", "/index"})
 	public String index()
 	{
 		return "index";
