0dbda04c40f19e4503dba07c179e8c3558731c82
Aliaksei Budavei <oytis@yandex.ru>
2018-03-26 20:38:52 +0300

    Create a LoanDao interface
    This LoanDao interface should extend the PagingAndSortingRepository
    interface.

---
+++ armaria-dao/src/main/java/org/example/zzzyxwvut/armaria/dao/LoanDao.java
@@ -0,0 +1,9 @@
+package org.example.zzzyxwvut.armaria.dao;
+
+import org.example.zzzyxwvut.armaria.domain.Loan;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface LoanDao extends PagingAndSortingRepository<Loan, Long>
+{
+
+}
