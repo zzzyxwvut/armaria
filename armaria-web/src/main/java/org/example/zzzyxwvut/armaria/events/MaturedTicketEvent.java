073d4b934a3602e9e5f3b6267fcca11bea04a29e
Aliaksei Budavei <oytis@yandex.ru>
2018-04-09 20:10:56 +0300

    Create a MaturedTicketEvent class
    This MaturedTicketEvent class should extend the UserEvent class.

---
+++ armaria-web/src/main/java/org/example/zzzyxwvut/armaria/events/MaturedTicketEvent.java
@@ -0,0 +1,40 @@
+package org.example.zzzyxwvut.armaria.events;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Objects;
+
+import org.example.zzzyxwvut.armaria.beans.TicketBean;
+
+public final class MaturedTicketEvent extends UserEvent
+{
+	private static final long serialVersionUID	= 8551036653037673802L;
+	private final transient Long loanId;
+	private final transient TicketBean ticket;
+	private final transient Object lock;
+
+	private void writeObject(ObjectOutputStream s) throws IOException
+	{
+		s.defaultWriteObject();
+	}
+
+	private void readObject(ObjectInputStream s) throws IOException,
+							ClassNotFoundException
+	{
+		s.defaultReadObject();
+	}
+
+	public MaturedTicketEvent(Long loanId, TicketBean ticket,
+						Object lock, String locale)
+	{
+		super(ticket.getUser(), locale);
+		this.loanId	= Objects.requireNonNull(loanId);
+		this.ticket	= Objects.requireNonNull(ticket);
+		this.lock	= lock;
+	}
+
+	public Long getLoanId()		{ return loanId; }
+	public TicketBean getTicket()	{ return ticket; }
+	public Object getLock()		{ return lock; }
+}
