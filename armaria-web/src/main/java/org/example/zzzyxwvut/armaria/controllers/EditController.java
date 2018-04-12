e53b50dc4de97ee4af084f2500d35bb844120e91
Aliaksei Budavei <oytis@yandex.ru>
2018-04-12 19:18:15 +0300

    Create an EditController class

---
+++ armaria-web/src/main/java/org/example/zzzyxwvut/armaria/controllers/EditController.java
@@ -0,0 +1,106 @@
+package org.example.zzzyxwvut.armaria.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.zzzyxwvut.armaria.annotation.AuthenticatedPersistentUserBean;
+import org.example.zzzyxwvut.armaria.beans.UserBean;
+import org.example.zzzyxwvut.armaria.crypto.Encryptor;
+import org.example.zzzyxwvut.armaria.security.beans.PersistentUserBean;
+import org.example.zzzyxwvut.armaria.security.events.PrincipalUpdatedEvent;
+import org.example.zzzyxwvut.armaria.service.UserService;
+import org.example.zzzyxwvut.armaria.validators.OldUserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@PreAuthorize("isAuthenticated()")
+@RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
+public class EditController
+{
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private OldUserValidator oldUserValidator;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	private final Logger logger	= LogManager.getLogger();
+
+	@InitBinder("dummyUser")
+	protected void initBinder(WebDataBinder binder)
+	{
+		binder.setValidator(oldUserValidator);
+	}
+
+	@GetMapping("/edit")
+	public String edit()	{ return "edit"; }
+
+	@PostMapping("/update")
+	public String update(@Validated @ModelAttribute("dummyUser") UserBean dummyUser,
+				BindingResult result, RedirectAttributes attr,
+				@AuthenticatedPersistentUserBean PersistentUserBean user,
+				@RequestParam("copyWord") String password)
+	{
+		if (password == null || !dummyUser.getPassword().equals(password))
+			result.rejectValue("password", "password.other");
+
+		if (result.hasErrors()) {
+			logger.debug(result);
+			attr.addFlashAttribute("org.springframework.validation.BindingResult.dummyUser",
+								result);
+			attr.addFlashAttribute("dummyUser", dummyUser);
+			attr.addFlashAttribute("copyWord", password);
+			return "redirect:/edit";
+		}
+
+		/* Ignore the cached UserDetailsBean instance. */
+		UserBean currentUser	= userService.getUserByLogin(user.getLogin());
+		boolean changed		= false;
+
+		if (dummyUser.getPassword() != null
+				&& dummyUser.getPassword().length() > 0) {
+			currentUser.setPassword(
+				Encryptor.INSTANCE.encrypt(dummyUser.getPassword()));
+			changed	= true;
+		}
+
+		if (dummyUser.getEmail() != null
+				&& dummyUser.getEmail().length() > 0
+				&& !dummyUser.getEmail().equals(user.getEmail())) {
+			currentUser.setEmail(dummyUser.getEmail());
+			changed	= true;
+		}
+
+		if (!changed)
+			return "redirect:/edit";
+
+		userService.saveUser(currentUser);
+		publisher.publishEvent(new PrincipalUpdatedEvent(currentUser));
+		return "redirect:/profile";
+	}
+
+	@ModelAttribute("dummyUser")
+	public UserBean copyOldUser(@AuthenticatedPersistentUserBean PersistentUserBean user)
+	{
+		UserBean dummy	= new UserBean();
+		dummy.setLogin(user.getLogin());
+		dummy.setPassword("");
+		dummy.setEmail(user.getEmail());
+		return dummy;
+	}
+}
